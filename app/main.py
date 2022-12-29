ad86eaec248cc11405b67741174f19afe8fb5db0
Sarith Ravinaka Imaduwage <imaduwagesarith932@gmail.com>
2022-12-29 23:27:45 +0530

    Added file uploading endpoint.

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 import os
-from fastapi import FastAPI, Depends, Request
-from fastapi.responses import HTMLResponse
+import io
+import uuid
+from fastapi import (FastAPI,
+                    Depends,
+                    Request,
+                    File,
+                    UploadFile,
+                    HTTPException)
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
 from functools import lru_cache
 
 class Settings(BaseSettings):
     debug:bool = False
+    echo_active:bool = False
 
     class Config:
         env_file = ".env"
@@ -19,6 +27,7 @@ settings = get_settings()
 DEBUG = settings.debug
 
 BASE_DIR = os.path.dirname(__file__)
+UPLOAD_DIR = f"{BASE_DIR}/uploaded"
 app = FastAPI()
 templates = Jinja2Templates(directory = f"{BASE_DIR}/templates")
 
@@ -29,3 +38,19 @@ def home_view(request: Request, settings:Settings = Depends(get_settings)):
 @app.post("/") # http POST
 def home_detail_view():
     return {"hello":"world"}
+
+@app.post("/img_echo",response_class=FileResponse)
+
+async def home_detail_view(file:UploadFile= File(...)):
+
+    if not settings.echo_active:
+        raise HTTPException(status_code = 404, detail= 'Invalid endpoint.')
+
+    file_bytes_str = io.BytesIO(await file.read())
+    # save the byte stream
+    fname = os.path(file.filename)
+    fsuffix = str(fname).suffix
+    dest = f"{UPLOAD_DIR}/{uuid.uuid1()}{fsuffix}"
+    with open(dest, 'wb') as out:
+        out.write(file_bytes_str.read())
+    return dest
